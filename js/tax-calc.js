dd84544aa6702b35b69ab78fcd09f3c2f4245a5e
Sam Lachance <slachance@gmail.com>
2015-09-29 19:30:03 -0400

    fixed bugs in tax-calc.js and changed styling in the try-it section

--- js/tax-calc.js
+++ js/tax-calc.js
@@ -5,35 +5,46 @@ $('input').keyup(function(){ // run anytime the value changes
 
     // First bracket calculation
     if (income > 9225) {
-      $('#firstBracket').html(9225 * .10);
+      $('#firstBracket').html((9225 * .10).toFixed(2));
+    }
+    else if ((income * 1) != income) {
+      $('#firstBracket').html((0).toFixed(2))
     }
     else {
-      $('#firstBracket').html(income * .10);
+      $('#firstBracket').html((income * .10).toFixed(2));
     }
 
     // Second bracket calculation
     if (income < 9226) {
-      $('#secondBracket').html(0);
+      $('#secondBracket').html((0).toFixed(2));
     }
 
     else if (income < 37450) {
-      $('#secondBracket').html((income - 9225) * .15);
+      $('#secondBracket').html(((income - 9225) * .15).toFixed(2));
+    }
+
+    else if ((income * 1) != income) {
+      $('#secondBracket').html((0).toFixed(2))
     }
 
     else {
-      $('#secondBracket').html((37450 - 9225) * .15);
+      $('#secondBracket').html(((37450 - 9225) * .15).toFixed(2));
     }
 
     // Third bracket calculation
     if (income < 37451) {
-      $('#thirdBracket').html(0);
+      $('#thirdBracket').html((0).toFixed(2));
     }
 
     else if (income < 89350) {
-      $('#thirdBracket').html((income -37450) * .25)
+      $('#thirdBracket').html(((income -37450) * .25).toFixed(2))
+    }
+
+    else if ((income * 1) != income) {
+      $('#thirdBracket').html((0).toFixed(2))
     }
 
     else {
-      $('#thirdBracket').html((89350 - 37450) * .25);
+      $('#thirdBracket').html(((89350 - 37450) * .25).toFixed(2));
     }
 });
